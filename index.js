84e34b885653bc9f18f5af51eb9b8db0666da013
Emilio <marqetm@gmail.com>
2022-05-29 20:43:54 -0500

    commit tree fcc066e04cb5a7ba82907cc7279cb98e1748a3a8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, strg) {
+  return drivers.filter((drivers) => {
+    return drivers.toLowerCase() === strg.toLowerCase()
+  })
+}
+
+function fuzzyMatch(drivers, strg) {
+  return drivers.filter(driver => {
+    return driver.toLowerCase().substring(0, strg.length) === strg.toLowerCase()
+  })
+}
+
+function matchName(drivers, strg) {
+  return drivers.filter(driver => {
+    return driver.name === strg
+  })
+}
